9803f4a99ab38e9c9c4f642dc17c12abbdde3f9c
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-31 13:24:09 +0530

    [ SANDIP ] Adding  ' Maximum Number Using String No '

---
+++ MaximumProblemusingGenerics/MaxStringNo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumProblemusingGenerics
+{
+    internal class MaxStringNo
+    {
+        public static string maxStringNo(string firstString, string secondString, string thirdString)
+        {
+            if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
+                firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
+            {
+                return firstString;
+            }
+
+            if (secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) >= 0 && secondString.CompareTo(thirdString) > 0 ||
+                secondString.CompareTo(firstString) > 0 && secondString.CompareTo(thirdString) >= 0)
+            {
+                return secondString;
+            }
+
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) >= 0 && thirdString.CompareTo(secondString) > 0 ||
+                thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(secondString) >= 0)
+            {
+                return thirdString;
+            }
+
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+    }
+}
--- MaximumProblemusingGenerics/Program.cs
+++ MaximumProblemusingGenerics/Program.cs
@@ -5,11 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("!!!!! WELCOME Maximum Problem using Generics !!!!!");
-            Console.WriteLine("Find max Number");
-            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
-            Console.WriteLine(output);
-            double doubleoutput = MaxFloatsNo.maxFloatsNo(111.2, 22.45, 78.56);
-             Console.WriteLine(doubleoutput);
+            //Console.WriteLine("Find max Number");
+          //  int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            //Console.WriteLine(output);
+            //double doubleoutput = MaxFloatsNo.maxFloatsNo(111.2, 22.45, 78.56);
+             //Console.WriteLine(doubleoutput);
+            string strigoutput = MaxStringNo.maxStringNo("22", "33", "44");
+              Console.WriteLine(strigoutput);
         }
     }
 }
