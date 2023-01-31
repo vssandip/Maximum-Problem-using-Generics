f247d3aec0ebf710a3911bc4b85eceafa2e179d8
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-31 13:44:35 +0530

    [ SANDIP ] Adding  ' More Then Three Numbers '

---
+++ MaximumProblemusingGenerics/MoreThen3Paramter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumProblemusingGenerics
+{
+        public class GenericMaximum<T> where T : IComparable
+        {
+            public T[] value;
+            public GenericMaximum(T[] value)
+            {
+                this.value = value;
+            }
+
+            public T[] Sort(T[] values)
+            {
+                Array.Sort(values);
+                return values;
+
+            }
+
+            void add(params int[] a)
+            {
+
+            }
+
+
+            public T MaxValue(params T[] values)
+            {
+                var sorted_values = Sort(values);
+
+                return sorted_values[sorted_values.Length - 1];
+            }
+
+            public T MaxMethod()
+            {
+                var max = MaxValue(this.value);
+                return max;
+            }
+
+            public void PrintMaxValue()
+            {
+                var max = MaxValue(this.value);
+                Console.WriteLine("Maximum value is " + max);
+            }
+        }
+    
+}
--- MaximumProblemusingGenerics/Program.cs
+++ MaximumProblemusingGenerics/Program.cs
@@ -6,12 +6,21 @@
         {
             Console.WriteLine("!!!!! WELCOME Maximum Problem using Generics !!!!!");
             //Console.WriteLine("Find max Number");
-          //  int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            //  int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
             //Console.WriteLine(output);
             //double doubleoutput = MaxFloatsNo.maxFloatsNo(111.2, 22.45, 78.56);
-             //Console.WriteLine(doubleoutput);
-            string strigoutput = MaxStringNo.maxStringNo("22", "33", "44");
-              Console.WriteLine(strigoutput);
+            //Console.WriteLine(doubleoutput);
+            // string strigoutput = MaxStringNo.maxStringNo("22", "33", "44");
+            //  Console.WriteLine(strigoutput);
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "111", "222", "333", "55", "999" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
         }
     }
 }
