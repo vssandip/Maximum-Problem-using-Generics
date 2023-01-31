c0a831427f632482b8d26d10d01feefccad761a1
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-31 13:15:43 +0530

    [ SANDIP ] Adding  ' Maximum Number Using Float No '

---
+++ MaximumProblemusingGenerics/MaxFloatsNo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumProblemusingGenerics
+{
+    internal class MaxFloatsNo
+    {
+        public static double maxFloatsNo(double firstValue, double secondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+    }
+}
--- MaximumProblemusingGenerics/Program.cs
+++ MaximumProblemusingGenerics/Program.cs
@@ -8,6 +8,8 @@
             Console.WriteLine("Find max Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
             Console.WriteLine(output);
+            double doubleoutput = MaxFloatsNo.maxFloatsNo(111.2, 22.45, 78.56);
+             Console.WriteLine(doubleoutput);
         }
     }
 }
