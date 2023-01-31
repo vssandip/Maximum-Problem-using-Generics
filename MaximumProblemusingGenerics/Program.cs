d418cfebd1e52f50c754451a01e5c375b7a5498c
“Vssandip” <sandipshindesr95035@gmail.com>
2023-01-31 12:45:31 +0530

    [ SANDIP ] Adding  ' Generics Problem '

---
+++ MaximumProblemusingGenerics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MaximumProblemusingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
